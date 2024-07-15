77f24c057c5edb3ef88ee446c8c9fc01a59ad34b
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-07-15 22:18:09 +0530

    stream controller example added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,58 +14,55 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true),
+      home: StreamControllerExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class StreamControllerExample extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _StreamControllerExampleState createState() =>
+      _StreamControllerExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _StreamControllerExampleState extends State<StreamControllerExample> {
+  final StreamController<int> _controller = StreamController<int>();
   int _counter = 0;
 
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
+  }
+
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    _counter++;
+    _controller.sink.add(_counter);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        StreamBuilder<int>(
+          stream: _controller.stream,
+          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+            if (snapshot.hasData) {
+              return Text('Counter: ${snapshot.data}');
+            } else {
+              return const Text('Counter: 0');
+            }
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: _incrementCounter,
+          child: const Text('Increment Counter'),
+        ),
+      ],
     );
   }
 }
